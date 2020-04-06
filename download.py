cb0a05db48fad39eaee6d3bf68ff95b559f86073
vanekm <57401333+vanekm@users.noreply.github.com>
2020-04-06 11:19:38 +0200

    pripravit download na makeprijem a prepsat do noveho Pythonu

--- download.py
+++ download.py
@@ -1,31 +1,21 @@
 import os, os.path, datetime, subprocess
 
-UPLOAD_DIR = "/network/data/www/fykos/db.fykos.cz/upload/fykos/"
-DOWNLOAD_DIR = "./download/"
-TEMP_DIR = "./temp"
-
-def download(username, rocnik, serie):
-    download_path = os.path.join(DOWNLOAD_DIR,
-                                            'rocnik%d' % rocnik, 
-                                            'serie%d' % serie)
+def download(upload_path, download_path, username, temp_path = "./temp"):
+    """odkud kam kdo stahuje"""
     if not os.path.exists(download_path):
         os.makedirs(download_path)
-    if not os.path.exists(TEMP_DIR):
-        os.makedirs(TEMP_DIR)
-    command = "scp -r %s@fykos.cz:%s %s" % ( 
-                                        username, 
-                                        os.path.join(UPLOAD_DIR, 
-                                            'rocnik%d' % rocnik, 
-                                            'serie%d' % serie, 
-                                            '*'),
-                                        download_path
-                                        )
+    if not os.path.exists(temp_path):
+        os.makedirs(temp_path)
+    command = f"scp -r {username}@fykos.cz:{upload_path} {download_path}"
     os.system(command)
     
 if __name__ == "__main__":
-
+    
     rocnik = int(input('napis cislo rocniku: '))
     serie = int(input('napis cislo serie: '))
-    login = input('napis login na server: ')
+    username = input('napis login na server: ')
 
-    download(login, rocnik, serie)
+    download_path = f"./download/rocnik{rocnik}/serie{serie}"
+    upload_path = f"/network/data/www/fykos/db.fykos.cz/upload/fykos/rocnik{rocnik}/serie{serie}/*"
+
+    download(upload_path, download_path, username)
